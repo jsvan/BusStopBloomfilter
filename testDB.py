f6d9e11aa107967cd0cb42e979fef455045cdd5c
Julian Vanecek <julian.vanecek@gmail.com>
2023-03-21 10:13:27 -0700

    Update testDB.py

--- testDB.py
+++ testDB.py
@@ -1,13 +1,13 @@
 import deutschebahn
 import pickle
 
-with open('/Users/onion/Documents/Classes/pickles/allstations.pkl', 'rb') as F:
+with open('./pickles/allstations.pkl', 'rb') as F:
     allstations = pickle.load(F)
 
-with open('/Users/onion/Documents/Classes/pickles/alltimetables.pkl', 'rb') as F:
+with open('./pickles/alltimetables.pkl', 'rb') as F:
     alltimetables = pickle.load(F)
 
-with open('/Users/onion/Documents/Classes/pickles/alltrains.pkl', 'rb') as F:
+with open('./pickles/alltrains.pkl', 'rb') as F:
     alltrains = pickle.load(F)
 
 germany = deutschebahn.System(alltrains, allstations, alltimetables, max_bloom_elements=30, failure_rate=0.01)
